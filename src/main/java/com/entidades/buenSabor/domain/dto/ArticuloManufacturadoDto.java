ad2a9b1e9fdb7e25de190e8ad7e46da2ec715c81
roAscurra <rominaascurra4887@gmail.com>
2024-05-19 17:51:36 -0300

    Mod

---
+++ src/main/java/com/entidades/buenSabor/domain/dto/ArticuloManufacturadoDto.java
@@ -0,0 +1,16 @@
+package com.entidades.buenSabor.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ArticuloManufacturadoDto extends BaseDto {
+    private String descripcion;
+    private Integer tiempoEstimadoMinutos;
+    private String preparacion;
+}
