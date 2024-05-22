414b174baf65228e7e0ba794ce6f80531437c328
roAscurra <rominaascurra4887@gmail.com>
2024-05-22 16:09:49 -0300

    Creo una empresa

--- src/main/java/com/entidades/buenSabor/domain/dto/sucursal/SucursalFullDto.java
+++ src/main/java/com/entidades/buenSabor/domain/dto/sucursal/SucursalFullDto.java
@@ -4,6 +4,7 @@ import com.entidades.buenSabor.domain.dto.BaseDto;
 import com.entidades.buenSabor.domain.dto.domicilio.DomicilioFullDto;
 import com.entidades.buenSabor.domain.dto.empresa.EmpresaCreateDto;
 import com.entidades.buenSabor.domain.dto.imagen.ImagenDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +20,14 @@ import java.util.Set;
 public class SucursalFullDto extends BaseDto {
 
     private String nombre;
+    @Schema(type = "string", format = "time", pattern = "HH:mm", description = "Horario de apertura en formato HH:mm")
     private LocalTime horarioApertura;
+    @Schema(type = "string", format = "time", pattern = "HH:mm", description = "Horario de cierre en formato HH:mm")
     private LocalTime horarioCierre;
     private boolean esCasaMatriz;
 
     private DomicilioFullDto domicilio;
     private EmpresaCreateDto empresa;
-    private Set<ImagenDto> imagenes;
+//    private Set<ImagenDto> imagenes;
 
 }
