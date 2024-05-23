bd1c70277ec4961f6fda7315aa77a1006658aec4
roAscurra <rominaascurra4887@gmail.com>
2024-05-22 22:05:43 -0300

    NO SIRVE

--- src/main/java/com/entidades/buenSabor/business/mapper/ArticuloInsumoMapper.java
+++ src/main/java/com/entidades/buenSabor/business/mapper/ArticuloInsumoMapper.java
@@ -1,9 +1,14 @@
 package com.entidades.buenSabor.business.mapper;
 
+import com.entidades.buenSabor.business.service.UnidadMedidaService;
 import com.entidades.buenSabor.domain.dto.articuloInsumo.ArticuloInsumoFullDto;
 import com.entidades.buenSabor.domain.entities.ArticuloInsumo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel =  "spring", uses = LocalidadMapper.class)
+@Mapper(componentModel = "spring", uses = {UnidadMedidaService.class, ImagenArticuloMapper.class})
 public interface ArticuloInsumoMapper extends BaseMapper<ArticuloInsumo, ArticuloInsumoFullDto> {
+    @Mapping(target = "unidadMedida", source = "idUnidadMedida",qualifiedByName = "getById")
+    @Mapping(target = "habilitado", constant = "true")
+    ArticuloInsumo toEntityCreate(ArticuloInsumoFullDto source);
 }
--- src/main/java/com/entidades/buenSabor/business/mapper/LocalidadMapper.java
+++ src/main/java/com/entidades/buenSabor/business/mapper/LocalidadMapper.java
@@ -3,7 +3,13 @@ package com.entidades.buenSabor.business.mapper;
 import com.entidades.buenSabor.domain.dto.localidad.LocalidadFullDto;
 import com.entidades.buenSabor.domain.entities.Localidad;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface LocalidadMapper extends BaseMapper<Localidad, LocalidadFullDto> {
+    @Mapping(target = "provincia", source = "idProvincia",qualifiedByName = "getById")
+    Localidad toEntityCreate(LocalidadFullDto source);
+    @Named("getAll")
+    Localidad toEntity(LocalidadFullDto source);
 }
