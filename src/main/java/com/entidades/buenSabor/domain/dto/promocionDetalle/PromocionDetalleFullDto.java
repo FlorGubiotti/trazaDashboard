76ba7836025b78e3f7f31a48cabc74f7a82c01b6
roAscurra <rominaascurra4887@gmail.com>
2024-05-25 15:06:54 -0300

    Promocion

--- src/main/java/com/entidades/buenSabor/domain/dto/promocionDetalle/PromocionDetalleFullDto.java
+++ src/main/java/com/entidades/buenSabor/domain/dto/promocionDetalle/PromocionDetalleFullDto.java
@@ -1,10 +1,14 @@
 package com.entidades.buenSabor.domain.dto.promocionDetalle;
 
 import com.entidades.buenSabor.domain.dto.BaseDto;
+import com.entidades.buenSabor.domain.dto.articuloManufacturado.ArticuloManufacturadoFullDto;
+import com.entidades.buenSabor.domain.entities.ArticuloManufacturado;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +16,5 @@ import lombok.Setter;
 @Setter
 public class PromocionDetalleFullDto extends BaseDto {
     private Integer cantidad;
+    private Set<ArticuloManufacturadoFullDto> articulosManufacturados;
 }
