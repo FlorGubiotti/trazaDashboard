f4bd9f0dc98bca52a9c6e9f7d19bae73f6ef0b11
roAscurra <rominaascurra4887@gmail.com>
2024-05-27 09:15:35 -0300

    Modificacion

--- src/main/java/com/entidades/buenSabor/domain/entities/Promocion.java
+++ src/main/java/com/entidades/buenSabor/domain/entities/Promocion.java
@@ -36,7 +36,7 @@ public class Promocion  extends Base{
     @JoinColumn(name = "promocion_id")
     //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
     @Builder.Default
-    private Set<PromocionDetalle> promocionDetalles = new HashSet<>();
+    private Set<PromocionDetalle> promocionDetalle = new HashSet<>();
 
     @OneToMany
     //SE AGREGA EL JOIN COLUMN PARA QUE JPA NO CREE LA TABLA INTERMEDIA EN UNA RELACION ONE TO MANY
