01dd5f8b97554f3ee5e1ec769b010314f53d484c
conito0 <unoconstanza@gmail.com>
2024-06-08 16:31:58 -0300

    usuario por email

--- src/main/java/com/entidades/buenSabor/business/facade/UsuarioFacade.java
+++ src/main/java/com/entidades/buenSabor/business/facade/UsuarioFacade.java
@@ -2,6 +2,10 @@ package com.entidades.buenSabor.business.facade;
 
 import com.entidades.buenSabor.business.facade.Base.BaseFacade;
 import com.entidades.buenSabor.domain.dto.usuarioCliente.UsuarioDto;
+import com.entidades.buenSabor.domain.entities.Usuario;
 
 public interface UsuarioFacade extends BaseFacade<UsuarioDto, Long> {
+
+    Usuario obtenerUsuarioPorEmail(String email);
+
 }
