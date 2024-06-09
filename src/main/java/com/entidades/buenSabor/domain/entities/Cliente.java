9114d06a529e4d6690e0175e13029fa6f5be11f9
roAscurra <rominaascurra4887@gmail.com>
2024-06-09 16:23:00 -0300

    Correcion relacion

--- src/main/java/com/entidades/buenSabor/domain/entities/Cliente.java
+++ src/main/java/com/entidades/buenSabor/domain/entities/Cliente.java
@@ -27,21 +27,21 @@ public class Cliente extends Base{
     @OneToOne
     private Usuario usuario;
 
-    @OneToOne
-    @NotAudited
-    private ImagenCliente imagenCliente;
-
-    @ManyToMany
-    //SE AGREGA EL JOIN TABLE PARA QUE JPA CREE LA TABLA INTERMEDIA EN UNA RELACION MANY TO MANY
-    @JoinTable(name = "cliente_domicilio",
-            joinColumns = @JoinColumn(name = "cliente_id"),
-            inverseJoinColumns = @JoinColumn(name = "domicilio_id"))
-    //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
-    @Builder.Default
-    private Set<Domicilio> domicilios = new HashSet<>();
-
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
-    //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
-    @Builder.Default
-    private Set<Pedido> pedidos = new HashSet<>();
+//    @OneToOne
+//    @NotAudited
+//    private ImagenCliente imagenCliente;
+//
+//    @ManyToMany
+//    //SE AGREGA EL JOIN TABLE PARA QUE JPA CREE LA TABLA INTERMEDIA EN UNA RELACION MANY TO MANY
+//    @JoinTable(name = "cliente_domicilio",
+//            joinColumns = @JoinColumn(name = "cliente_id"),
+//            inverseJoinColumns = @JoinColumn(name = "domicilio_id"))
+//    //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
+//    @Builder.Default
+//    private Set<Domicilio> domicilios = new HashSet<>();
+//
+//    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+//    //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
+//    @Builder.Default
+//    private Set<Pedido> pedidos = new HashSet<>();
 }
